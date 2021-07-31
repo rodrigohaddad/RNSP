7ea80af6266c9eeeeb8c69fe27c07dce8ec9a2ea
rodrigohaddad <rgh@poli.ufrj.br>
2021-07-31 11:13:33 -0300

    imgs

--- t2/merge-imgs.py
+++ t2/merge-imgs.py
@@ -2,17 +2,11 @@ import cv2
 
 
 def main():
-    im1 = cv2.imread('t2/images/address_mel_spectogram.png')
-    im2 = cv2.imread('t2/images/address_mfcc.png')
+    im1 = cv2.imread('t2/images/consusion-mel.png')
+    im2 = cv2.imread('t2/images/confusion-mfcc.png')
     im_h = cv2.hconcat(im1, im2)
 
-    cv2.imwrite('t2/images/address_concat.png', im_h)
-
-    im1 = cv2.imread('t2/images/secs-mel-spectogram.png')
-    im2 = cv2.imread('t2/images/secs-mfcc.png')
-    im_h = cv2.hconcat(im1, im2)
-
-    cv2.imwrite('t2/images/secs_concat.png', im_h)
+    cv2.imwrite('t2/images/confusion-concat.png', im_h)
 
 
 if __name__ == '__main__':
